294c9cb8e8447b3f5e3a4a20274a6517fb58129c
Berna Castro <bernacas@gmail.com>
2019-10-02 15:14:17 +0200

    Implement hot-reloader library

---
+++ lib/ak.rb
@@ -0,0 +1,169 @@
+require 'filewatcher'
+
+module Ak
+  extend self
+  attr_accessor :root
+
+  class Registry
+    attr_reader :files
+
+    def initialize
+      @files = []
+    end
+
+    def load(path, **options)
+      path = get_path(path)
+
+      file = Ak::HotFile.new(path, options)
+      file.load_file
+
+      files.push(file)
+    end
+
+    def find_by(type, value)
+      files.find { |file| file.send(type) == value }
+    end
+
+    private
+
+    def get_path(path)
+      path = "#{path}.rb" unless path.match(/.rb$/)
+      return path if path.include?(Ak.root)
+      "#{Ak.root}/#{path}"
+    end
+  end
+
+  class HotFile 
+    attr_reader :name, :const, :descendants
+
+    def initialize(name, **options)
+      @name        = get_name(name)
+      @const       = options[:const]
+      @require     = options[:require]
+      @descendants = []
+    end
+
+    def add_descendant(file)
+      @descendants << file
+    end
+
+    def load_file
+      require name if require_file?
+      set_const unless const
+      set_parents if const
+    end
+
+    def reload
+      unload
+      require name
+      descendants.each &:reload
+      true
+    end
+
+    def unload
+      if const
+        match = const.to_s.match(/\:\:(\w+)$/)
+
+        if match
+          parent = const.to_s.gsub("::#{match[1]}", '')
+          Object.const_get(parent).send(:remove_const, match[1])
+        else
+          Object.send(:remove_const, const.to_s)
+        end
+      end
+
+      $LOADED_FEATURES.delete(name)
+    end
+
+    def unload_and_reload
+      unload and descendants.each(&:reload)
+    end
+
+    private
+
+    def get_name(name)
+      File.expand_path(name, Ak.root)
+    end
+
+    def set_const
+      path = name.gsub(/#{Ak.root}\/|\.rb/, '').split('/')
+      consts = path.map do |el|
+        index = path.find_index { |_el| el == _el }
+        Inflector.camelize path[index..(path.count)].join('/')
+      end
+      valid_const = nil
+      
+      consts.each do |const|
+        valid_const = Object.const_get(const) rescue nil
+        break if valid_const
+      end
+
+      @const = valid_const
+    end
+
+    def set_parents
+      parents = const.ancestors.map do |ancestor|
+        unless ancestor == const
+          Ak.registry.find_by(:const, ancestor)
+        end
+      end.compact
+
+      parents.each do |parent|
+        parent.add_descendant(self)
+      end
+    end
+
+    def require_file?
+      @require && !$LOADED_FEATURES.include?(name)
+    end
+  end
+
+  class Inflector
+    def self.camelize(path)
+      path.gsub(/\/(.?)/) { "::" + $1.upcase }.gsub(/(^|_)(.)/) { $2.upcase }
+    end
+  end
+
+  def require(path, **options)
+    registry.load(path, options.merge(require: true))
+  end
+
+  def require_relative(path, **options)
+    caller_path = File.dirname(caller_locations.first.path)
+    path = File.expand_path(path, caller_path)
+    registry.load(path, options.merge(require: true))
+  end
+
+  def reload(path, **options)
+    registry.load(path, options.merge(require: false))
+  end
+
+  def start
+    dirs = registry.files.map { |file| File.dirname(file.name) }.uniq
+    filewatcher = Filewatcher.new(dirs)
+
+    Thread.new(filewatcher) do |filewatcher|
+      filewatcher.watch do |filename, event| 
+        begin
+          puts "\n\n#{event.capitalize} #{filename.gsub("#{root}/", '')}\n\n"
+
+          file = registry.find_by(:name, filename)
+
+          if file
+            file.load              if event == :created
+            file.reload            if event == :updated
+            file.unload_and_reload if event == :deleted
+          else
+            self.require(filename)
+          end
+        rescue Exception => e
+          puts "\nError on event #{event}:\n#{'=' * 20}\n#{e}\n"
+        end
+      end
+    end
+  end
+
+  def registry
+    @registry ||= Registry.new
+  end
+end
